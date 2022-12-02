ae5ce5cf7046d50bb9bfa4c3fd9e53abb9d3b01c
Deydson Costa <deydsonassuncao20@gmail.com>
2022-12-02 20:04:10 -0300

    correção sqlite3 inicial

--- sqlite.py
+++ sqlite.py
@@ -4,11 +4,11 @@ banco = sqlite3.connect("Banco_dados.db")
 
 cursor = banco.cursor()
 
-#cursor.execute("CREATE TABLE People(nome text, idade integer, telefone integer)")
+cursor.execute("CREATE TABLE People(nome text, idade integer, telefone integer)")
 
 cursor.execute("INSERT INTO People VALUES('Geovano', 29, 91983298535)")
 
-banco.commit
+banco.commit()
 
 #cursor.execute("SELECT * FROM People")
 #print(cursor.fetchall())
