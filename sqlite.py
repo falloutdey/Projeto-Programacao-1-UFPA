d2c342a037ef5816413b098ff1d9ad7e3f06d0c0
iurivinicius <116917506+iurivinicius@users.noreply.github.com>
2022-12-03 11:46:49 -0300

    update sqlite3
    código ajustado para o projeto.

--- sqlite.py
+++ sqlite.py
@@ -13,3 +13,12 @@ banco.commit()
 #cursor.execute("SELECT * FROM People")
 #print(cursor.fetchall())
 
+#--update--
+import sqlite3
+banco = sqlite3.connect('bancodata.db')
+cursor = banco.cursor()
+cursor.execute("CREATE TABLE registros (link_site text, link_img text, titulo text, resumo text)")
+cursor.execute("INSERT INTO registros VALUES('https://linkteste.org/dl/','testando.com/img-teste-prog-1','titulo1','resumo1 lalalala')")
+banco.commit()
+#cursor.execute("SELECT * FROM pessoas")
+#print(cursor.fetchall())
