55de65b29dd394234897fe346d1febafa9cdeb7f
Deydson Costa <deydsonassuncao20@gmail.com>
2022-12-13 16:13:25 -0300

    frontend config

--- ProjetoEscavador/EscavadorApp/views.py
+++ ProjetoEscavador/EscavadorApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from .backend import urls
+from .backend import urls, filtragem
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'filtragem': filtragem()})
 
 def gerenciador(request):
     return render(request, 'gerenciador.html', {'urls': urls})
